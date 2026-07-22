be7ecab6fdab3ef91e776d8b4ed00c45923b1c91
lieoric <129092606+lieoric@users.noreply.github.com>
2026-07-23 00:36:05 +1000

    Remove files uploaded to the wrong directory

--- scripts/bootstrap.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-Set-Location (Split-Path -Parent $PSScriptRoot)
-conda env update --name comp6441-ctf --file environment.yml --prune
-conda run --name comp6441-ctf flask --app ctf_app reset-db
--- scripts/check.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-Set-Location (Split-Path -Parent $PSScriptRoot)
-conda run --name comp6441-ctf flask --app ctf_app reset-db
-conda run --name comp6441-ctf pytest -q
